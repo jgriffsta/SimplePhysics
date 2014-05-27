450c26efb4ef349b96c0769e7adaa168467eb0ed
jgriffsta <john.griffith@live.co.uk>
2014-05-27 14:37:41 +0100

    Collision
    Added collisions. plus other fixes. Collisions needs work

--- src.js
+++ src.js
@@ -12,10 +12,12 @@ document.body.appendChild(canvas)
 //Options
 var period = 2000; // Milliseconds
 var amplitude=0.25;
-var boxH=100;
-var boxW=100;
-var maxInitalSpeed = 30;
+
+var length=100;
+var maxInitalSpeed = 60;
 var gravity = -9.8;
+var elasticSolid = 0.9;
+var elasticBall = 0.9;
 //Data
 var pos=[0,0];
 var mousePos=[0,0];
@@ -27,27 +29,41 @@ var canvasH=canvas.height;
 var	canvasW=canvas.width;
 
 
-
 var end = false; 
 function draw() { 
 	
 	ctx.clearRect(0,0,canvasW,canvasH);
-	drawRectangle(pos[0],pos[1],pos[0]+boxW,pos[1]+boxH);
+	
 	
 	drawLine(0,canvasH,mousePos[0],mousePos[1]);
 	
+	var mpp = canvasW/length;
+	
+	
+	//Drawing balls
 	for(var i = 0;i<ballObject.length;i++){
-		if(ballObject[0i].visible)drawCircle(ballObject[i].posX,canvasH-ballObject[i].posX,ballObject[i].radius);
+		if(ballObject[i].visible)drawCircle(ballObject[i].posX*mpp,(canvasH-ballObject[i].posY*mpp),ballObject[i].radius*mpp);
+		
+		//drawLine (ballObject[i].posX*meterPixX,canvasH-(ballObject[i].posY+ballObject[i].radius)*meterPixY,ballObject[i].posX*meterPixX,canvasH-meterPixY*(ballObject[i].velY));
+		drawText(ballObject[i].velY,ballObject[i].posX*mpp,canvasH-(ballObject[i].posY+ballObject[i].radius+2)*mpp)
+		var r = ballObject[i].radius;
+		//drawRectangle((ballObject[i].posX-r)*mpp,canvasH-(ballObject[i].posY-r)*mpp,(ballObject[i].posX+r)*mpp,canvasH-(ballObject[i].posY+r)*mpp);
+	
 	}
 	
-	ctx.font = "bold 16px Arial";
-	ctx.fillText(angleD,100,500);
+		
+	
+	//drawText(angleD,100,500)
 	
 	}
 
 	
+function drawText(t,x,y){
+	ctx.font = "bold 16px Arial";
+	ctx.fillText(t,x,y);
+}
 window.addEventListener('mousemove', userInterface, false);
-window.addEventListener('mouseclick', newObject, false);
+window.addEventListener('click', newObject, false);
 function userInterface(e){
 	var rect = canvas.getBoundingClientRect();
 	mousePos[0]=e.clientX - rect.left;
@@ -56,17 +72,18 @@ function userInterface(e){
 
 function newObject(e){
 	var length = mousePos[0]/Math.cos(angleR);
-	var maxLength = Math.pow(Math.pow(screenH,2)+Math.pow(screenW,2),0.5)
+	var maxLength = Math.pow(Math.pow(canvasH,2)+Math.pow(canvasW,2),0.5)
 	var power = length/maxLength * maxInitalSpeed;
 	
 	var cos = Math.cos(angleR)* power;
 	var sin = Math.sin(angleR)* power;
 	var time = new Date();
-	ball.push(0,0,cos,sin,0,gravity,10,time)
+	var radius = 2.5;
+	ballObject.push(new ball(0,radius,cos,sin,0,gravity,radius,1))
 	
 	
 }
-function ball(posX,posY,velX,velY,accX,accY,radius,time){
+function ball(posX,posY,velX,velY,accX,accY,radius){
 	this.posXstart = posX;
 	this.posYstart = posY;
 	this.posX = posX;
@@ -76,7 +93,7 @@ function ball(posX,posY,velX,velY,accX,accY,radius,time){
 	this.accX = accX;
 	this.accY = accY;
 	this.radius = radius;
-	this.startTime = time;
+	this.mass = 1;
 	this.visible = true;
 }
 	
@@ -91,19 +108,74 @@ function physics(){
 		angleR = Math.atan((canvasH-mousePos[1])/mousePos[0]);
 		angleD = angleR*180/Math.PI;
 	}
-	pos[0] = 0;
-	pos[1] = amplitude*canvasH*Math.sin(omega*time) + canvasH*0.5;
+	
 	
 	//free fall of objects
 	for (var i = 0;i<ballObject.length;i++){
-		var t = timeNow - ballobject[i].startTime;
-		ballObject[i].posX = t*ballObject[i].velX + ballObject[i].posXstart;
-		ballObject[i].posY = t*ballObject[i].velY + 0.5*Math.pow(t,2)*gravity + ballObject[i].posYstart ;
+		var t = (timeNow - timeStart)/1000;
+		ballObject[i].posX += t*ballObject[i].velX;
+		ballObject[i].posY += t*ballObject[i].velY ;
+		
+		if(ballObject[i].posY-ballObject[i].radius<0)ballObject[i].posY= ballObject[i].radius;
+		
+		ballObject[i].velX +=  t* ballObject[i].accX;
+		ballObject[i].velY +=  t* ballObject[i].accY;
+		
+		ballObject[i].accX =0;
+		ballObject[i].accY=gravity;
 		
 		
+		if (ballObject[i].posY - ballObject[i].radius  <= 0   ){
+			ballObject[i].velY= -ballObject[i].velY*elasticSolid;
+		}
+		for (var y = 0;y<ballObject.length;y++){
+			if (y == i) continue;
+			
+			if (distance2Point(ballObject[i].posX,ballObject[i].posY,ballObject[y].posX,ballObject[y].posY)<=ballObject[y].radius+ballObject[i].radius){
+				var change = collision(i,y);
+				ballObject[i].velX= change[0];
+				ballObject[i].velY= change[1];
+				ballObject[y].velX= change[2];
+				ballObject[y].velX= change[3];
+				console.log(change[0]+"  "+change[1]);
+			}
+		}
 	}
 	
+	
+	
 	draw();
+}
+
+function distance2Point(x,y,x1,y1){
+
+	distance = Math.pow(Math.pow(x-x1,2)+Math.pow(y-y1,2),0.5)
+	
+	return distance;
+}
+
+function collision(i,y){
+	ballA = ballObject[i];
+	ballB = ballObject[y];
+	var e  = elasticBall;
+	var theta = Math.atan((ballB.posY-ballA.posY)/(ballB.posX-ballA.posX))
+	var alpha = Math.PI-theta;
+	var u1 =  ballA.velX*Math.cos(theta)+ballA.velY*Math.cos(alpha); //needs compacting
+	var u2 = ballB.velX*Math.cos(theta)+ballB.velY*Math.cos(alpha);
+	var k1 = ballA.velX*Math.sin(alpha)-ballA.velY*Math.sin(theta);
+	var k2 = ballB.velX*Math.sin(alpha)-ballB.velY*Math.sin(theta);
+	var v1 = (u1*ballA.mass*(1-e)+ballB.mass*u2*(1+e))/(2*ballA.mass);
+	var v2 = (u1*ballA.mass*(1+e)+ballB.mass*u2*(1-e))/(2*ballB.mass);
+	var landaA=Math.atan(k1/v1)+theta;
+	var landaB=Math.atan(k2/v2)+theta;
+	
+	var velAX = v1*Math.cos(landaA)-k1*Math.sin(landaA);
+	var velBX= v2*Math.cos(landaB)-k2*Math.sin(landaB);
+	var velAY = v1*Math.sin(landaA)+k1*Math.cos(landaA);
+	var velBY = v2*Math.sin(landaB)+k2*Math.cos(landaB);
+	var result = [velAX,velAY,velBX,velBY];
+	return(result);
+	
 }
 	
 function drawRectangle(x,y,x1,y1){
@@ -113,7 +185,9 @@ function drawRectangle(x,y,x1,y1){
 }
 
 function drawLine(x,y,x1,y1){
+	ctx.lineWidth = 2;
 	ctx.beginPath();
+	
 	ctx.moveTo(x,y);
 	ctx.lineTo(x1,y1);
 	ctx.stroke();
@@ -124,8 +198,7 @@ function drawCircle(x,y,r){
 	ctx.arc(x, y, r, 0, 2 * Math.PI, false);
 	ctx.fillStyle = 'green';
 	ctx.fill();
-	ctx.lineWidth = 5;
-	ctx.strokeStyle = '#003300';
+	
 	ctx.stroke();
 }
 
@@ -138,15 +211,16 @@ window.requestAnimFrame = (function(callback) {
 	  
 	  
 
-      function animate() {
-        
-		physics();
-		
-        if(end) return null;
-		
-		
-        requestAnimFrame(function() {
-          animate();
-        });
-      }
-      animate();
+function animate() {
+
+	physics();
+
+	if(end) return null;
+
+
+	requestAnimFrame(function() {
+	  animate();
+	  timeStart = new Date();
+	});
+}
+animate();
